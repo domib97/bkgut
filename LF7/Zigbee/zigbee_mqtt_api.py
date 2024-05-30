194c4269f3f01206c22c9553bd13198d98635581
domib97 <dominik.boehm97@gmail.com>
2024-05-30 16:10:07 +0200

    changed something

--- LF7/Zigbee/zigbee_mqtt_api.py
+++ LF7/Zigbee/zigbee_mqtt_api.py
@@ -1,35 +1,19 @@
+import time
 import paho.mqtt.client as mqtt
 import requests
 
-# Configuration
+# MQTT-Config
 broker = "domipi"
 port = 1883
 topic = "zigbee/lamp"
 client_id = "Lampe_Pub_Sub"
 
+# Zigbee-Config
 deconz_api_url = "http://[zigbee_gateway_ip]:[port]/api/[your_api_key]"
-lamp_id = "1"  # This is the ID of your IKEA lamp in the Deconz system
+lamp_id = "1"
 
 
-# Define the MQTT client callbacks
-def on_connect(client, rc):
-    print("Connected to MQTT broker with result code " + str(rc))
-    client.subscribe(topic)
-
-
-def on_message(msg):
-    try:
-        payload = msg.payload.decode()
-        if payload.lower() == "on":
-            control_lamp(True)
-        elif payload.lower() == "off":
-            control_lamp(False)
-        else:
-            print(f"Unknown command: {payload}")
-    except Exception as e:
-        print(f"Error processing message: {e}")
-
-
+# Zigbee
 def control_lamp(turn_on):
     state = "on" if turn_on else "off"
     url = f"{deconz_api_url}/lights/{lamp_id}/state"
@@ -44,17 +28,37 @@ def control_lamp(turn_on):
         print(f"Error controlling the lamp: {e}")
 
 
+# MQTT
+def domi_mqtt():
+    def on_connect(client, rc):
+        print("Connected to MQTT broker with result code " + str(rc))
+        client.subscribe(topic)
+
+    def on_message(msg):
+        try:
+            payload = msg.payload.decode()
+            if payload.lower() == "on":
+                control_lamp(True)
+            elif payload.lower() == "off":
+                control_lamp(False)
+            else:
+                print(f"Unknown command: {payload}")
+        except Exception as e:
+            print(f"Error processing message: {e}")
+
+    obj_client = mqtt.Client()  # Client Objekterstellung
+
+    obj_client.on_connect = on_connect
+    obj_client.on_message = on_message
+
+    obj_client.connect(broker, port, 60)  # Verbindung zum Broker
+
+    obj_client.loop_forever()  # Endlosschleife
+
+
 def run():
     try:
-
-        client = mqtt.Client()  # Setup MQTT client
-        client.on_connect = on_connect
-        client.on_message = on_message
-
-        client.connect(broker, port, 60)  # Connect to the MQTT broker
-
-        client.loop_forever()  # Blocking call processes network traffic, dispatches callbacks, handles reconnecting lol
-
+        domi_mqtt()
     except KeyboardInterrupt:
         print("Program terminated by user.")
 
